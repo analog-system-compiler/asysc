f8a89225df337b2dadb9b98a22831a9da1be8b1c
Cyril Collineau <cyril.collineau@gmail.com>
2025-02-19 19:28:08 +0100

    Images update

--- doc/image.sh
+++ doc/image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-montage -resize "50%" images/orig/*_ac.png     -mode Concatenate -tile 2x  ac_analysis.png
-montage -resize "50%" images/orig/*_trans.png  -mode Concatenate -tile 2x  trans_analysis.png
+montage -resize "50%" images/*_ac.png     -mode Concatenate -tile 2x  ac_analysis.png
+montage -resize "50%" images/*_trans.png  -mode Concatenate -tile 2x  trans_analysis.png
