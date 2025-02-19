4ec10ffbf0cf474ba5cf858f659013a51222c68e
Cyril Collineau <cyril.collineau@b-com.com>
2025-02-19 12:56:49 +0100

    Year update in source headers

--- examples/circuit_base.py
+++ examples/circuit_base.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2024 The ASysC project                        
+# Copyright (C) 2006-2025 The ASysC project                        
 #                                                                    
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- examples/transient/NE555_slider/simulation.py
+++ examples/transient/NE555_slider/simulation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (C) 2006-2024 The ASysC project
+# Copyright (C) 2006-2025 The ASysC project
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
